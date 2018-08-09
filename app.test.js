84e38a96e41e24c049b015f4f700b4623e4c2655
Crainbows <Crainbows@users.noreply.github.com>
2018-08-09 20:24:24 +0100

    Added more HTTP route tests

--- app.test.js
+++ app.test.js
@@ -3,8 +3,67 @@
 var request = require('supertest');
 var app = require('./app');
 
-test('Root page loads', function (done) {
-    request(app)
+request = request(app);
+
+// Testing for players
+test('Player - root page returns 200', function (done) {
+    request
         .get('/')
         .expect(200, done)
+});
+
+test('Player - dm page returns 403', function (done) {
+    request
+        .get('/dm')
+        .expect(403, done)
+});
+
+test('Player - quit page returns 403', function (done) {
+    request
+        .get('/quit')
+        .expect(403, done)
+});
+
+
+// Testing for DMs
+test('DM - root page returns 200', function (done) {
+    request
+        .get('/')
+        .set('Host','localhost:3000')
+        .expect(200, done);
+});
+
+test('DM - dm page returns 200', function (done) {
+    request
+        .get('/dm')
+        .set('Host','localhost:3000')
+        .expect(200, done);
+});
+
+test('DM - quit page returns 200', function (done) {
+    request
+        .get('/quit')
+        .set('Host','localhost:3000')
+        .expect(200, done);
+});
+
+test('DM - send page returns 200 with Base64 coded data', function (done) {
+    request
+        .post('/send')
+        .send({imageData: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='})
+        .set('Host','localhost:3000')
+        .expect(200, {
+            'success': true,
+            'responseText': 'Image successfully uploaded'
+        }, done);
+});
+
+test('DM - send page returns 200 with no data', function (done) {
+    request
+        .post('/send')
+        .set('Host','localhost:3000')
+        .expect(200, {
+            'success': false,
+            'responseText': 'Image not uploaded successfully'
+        }, done);
 });
