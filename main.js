e6fd441a25b7bb396c0134a2b68f56fe2ca7b855
Crainbows <Crainbows@users.noreply.github.com>
2018-11-08 23:57:42 +0000

    Added player window

--- main.js
+++ main.js
@@ -8,5 +8,15 @@ let win;
 app.on('ready', () => {
   const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize;
   win = new BrowserWindow({width, height});
-  win.loadURL('http://localhost:3000');
+  win.loadURL('http://localhost:3000/dm');
+
+  let win2 = new BrowserWindow({ frame: false })
+  win2.on('close', () => { win2 = null });
+  win2.loadURL('http://localhost:3000');
+  win2.show();
+})
+
+app.on('exit', () => {
+  serverProc.exit();
+  win2.exit();
 })
